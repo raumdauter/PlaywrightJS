91b6f1ae71f3a16795a28f7bdb757a03633ad025
vietdeotuitotemauxanhcaro <hoangviet250999@gmail.com>
2026-02-09 00:09:32 +0700

    group test

---
+++ tests/Grouping.spec.js
@@ -0,0 +1,39 @@
+import { test, expect } from '@playwright/test';
+//test('Keyboard Actions', async ({ page }) => { 
+
+test.beforeAll(async () => {
+    console. log('this is before all Hook .... ')
+})
+
+test.afterAll(async () => {
+    console. log('this is after all Hook .... ')
+})
+
+test.beforeEach(async ({page}) => {
+    console. log('this is before each Hook .... ')
+})
+
+test.afterEach(async ({page}) => {
+    console. log('this is after each Hook .... ')
+})
+
+test.describe.only('Group 1', () => {
+
+    test('Test1', async({page})=>{
+    console. log('this is test 1 .... ')
+    })
+    test('Test2', async({page})=>{
+console. log('this is test 2 .... ')
+    })
+})
+
+test.describe.skip('Group 2', () => {
+
+    test('Test3', async({page})=>{
+    console. log('this is test 3 .... ')
+    })
+
+    test('Test4', async({page})=>{
+    console. log('this is test 4 .... ')
+    })
+})
